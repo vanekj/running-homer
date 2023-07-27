cbfe43023125c4a09fcd4b23b52f45c8d77ac5b5
vanekj <info@jakub-vanek.eu>
2023-07-27 23:21:56 +0200

    feat: update to build Mac app only

--- forge.config.js
+++ forge.config.js
@@ -3,21 +3,9 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
   plugins: [
     {
